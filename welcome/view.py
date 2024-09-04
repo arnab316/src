d9a9ad61fa8aa621a5e27b5ec9eeb71ab4cf10ce
arnab <arnabgolder7@gmail.com>
2024-09-05 01:29:46 +0530

    basic templatevrendering

--- welcome/view.py
+++ welcome/view.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def home_page_view(request, *args ,**kwargs):
-    return HttpResponse("<h1>Hello, world. You're at the home page.</h1>")
+    # return HttpResponse("<h1>Hello, world. You're at the home page.</h1>")
+    return render(request, 'home.html')
